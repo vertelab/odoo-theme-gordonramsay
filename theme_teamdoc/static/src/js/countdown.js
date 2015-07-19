621ef982cf19d3ce71eecf6412ee0bd1e1b564a7
christelle wehbe <wehbechristelle@gmail.com>
2015-07-19 12:40:42 +0200

    added photos and a hemlincoaching map

--- theme_teamdoc/static/src/js/countdown.js
+++ theme_teamdoc/static/src/js/countdown.js
@@ -1,11 +1,11 @@
-//~ var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
+function initCountdown(year, month, day, hour, min){
+	target_date = new Date(year, month, day, hour, min); // set the countdown date, (month 0-11)
+	//~ var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
 
-var countdown = document.getElementById("tiles"); // get tag element
+	getCountdown();
 
-getCountdown();
-
-setInterval(function () { getCountdown(); }, 1000);
+	setInterval(function () { getCountdown(); }, 1000);
+}
 
 function getCountdown(){
 
@@ -13,15 +13,16 @@ function getCountdown(){
 	var current_date = new Date().getTime();
 	var seconds_left = (target_date - current_date) / 1000;
 
-	days = pad( parseInt(seconds_left / 86400) );
+	var days = pad( parseInt(seconds_left / 86400) );
 	seconds_left = seconds_left % 86400;
 		 
-	hours = pad( parseInt(seconds_left / 3600) );
+	var hours = pad( parseInt(seconds_left / 3600) );
 	seconds_left = seconds_left % 3600;
 		  
-	minutes = pad( parseInt(seconds_left / 60) );
+	var minutes = pad( parseInt(seconds_left / 60) );
 	seconds = pad( parseInt( seconds_left % 60 ) );
 
+	var countdown = document.getElementById("tiles"); // get tag element
 	// format countdown string + set tag value
 	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
 }
